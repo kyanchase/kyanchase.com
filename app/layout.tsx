3199526b4242170011ad3c21f10824f045304e83
kyanchase <165103120+kyanchase@users.noreply.github.com>
2026-08-02 15:25:52 -0400

    Add privacy-friendly site analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -36,7 +37,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Script
+          id="cloudflare-web-analytics"
+          type="module"
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          data-cf-beacon='{"token":"5fc6bdbc74a346049c45dade1c6955ff"}'
+          strategy="afterInteractive"
+        />
+      </body>
     </html>
   );
 }
